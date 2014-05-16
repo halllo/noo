212b3ae77704caeb0dd229c0004f8e9860cf9e29
Manuel Naujoks <mnaujoks@gmail.com>
2014-05-16 18:20:09 +0200

    index and mongourl separated.

---
+++ backend/db/mongodburl.js
@@ -0,0 +1,34 @@
+if(process.env.VCAP_SERVICES)
+{
+    var env = JSON.parse(process.env.VCAP_SERVICES);
+    var mongo = env['mongodb2-2.4.8'][0]['credentials'];
+}
+else
+{
+    var mongo = {
+    "hostname":"localhost",
+    "port":27017,
+    "username":"",
+    "password":"",
+    "name":"",
+    "db":"db"
+    }
+}
+var generate_mongo_url = function(obj){
+    obj.hostname = (obj.hostname || 'localhost');
+    obj.port = (obj.port || 27017);
+    obj.db = (obj.db || 'test');
+    if(obj.username && obj.password){
+        return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+    }
+    else{
+        return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+    }
+}
+
+var generate = function() {
+    //return generate_mongo_url(mongo);
+    return 'mongodb://localhost/meetappprototype';
+}
+
+module.exports.generate = generate;
